ba4cd34a5937900c101255e368096c6d72b3db82
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-06-26 14:09:08 +0000

    baseUrl 設定

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      return {};
-    },
+    baseUrl: "http://localhost:19006",
   },
 });
