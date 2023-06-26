55334d9e252c2483e29bdd75312b83357afb49fd
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-06-26 14:09:41 +0000

    #753 初期テスト作成

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,42 @@
-describe("spec.cy.ts", () => {
-  it("should pass", () => {
-    cy.visit("http://localhost:19006/");
-    expect(true).to.equal(true);
-    cy.screenshot();
+describe("CPA001HelloSpelieve", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("onPress_createItineraryButton", () => {
+    // take a snapshot before clicking the button
+    cy.get('[data-testid="createItineraryButton"]');
+    cy.screenshot("onPress_createItineraryButton-before");
+
+    // Click onPress
+    cy.get('[data-testid="createItineraryButton"]').click();
+
+    // Verify the URL
+    cy.url().should("include", "/ItineraryEdit");
+
+    // take a snapshot after clicking the button
+    cy.get('[data-testid="planPressable"]');
+    cy.screenshot("onPress_createItineraryButton-after");
+  });
+});
+describe("IPA001ItineraryEdit", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get('[data-testid="createItineraryButton"]').click();
+  });
+  it("onPress_planPressable", () => {
+    // take a snapshot before clicking the button
+    cy.get('[data-testid="planPressable"]');
+    cy.screenshot("onPress_planPressable-before");
+
+    // Click onPress
+    cy.get('[data-testid="planPressable"]').click();
+
+    // Verify the URL
+    cy.url().should("include", "/EditPlan");
+
+    // take a snapshot after clicking the button
+    cy.get('[data-testid="planThumbnailPressable"]');
+    cy.screenshot("onPress_planPressable-after");
   });
 });
